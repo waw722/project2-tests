4035abde9a1458956dd4894ce64b3104da17b2a6
William Lin <wlsaidhi@gmail.com>
2017-12-13 00:46:12 -0600

    aligned numbers

--- bug_test.c
+++ bug_test.c
@@ -10,6 +10,6 @@ int main() {
     double x, y;
     x = -2.1073424255447017e-08;
     y = asinh(x);
-    printf("y = %20.16e\n",y);
+    printf("y =       %20.16e\n",y);
     printf("should be -2.1073424255447017e-08\n");
 }
